f68017c834b6571bc0d223d168f446be2340084a
Stephanie Solórzano <stephaniesolorzano@192.168.1.4>
2023-06-18 00:10:49 -0300

    updates

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import Finalfinal from "../../img/finalfinal.jpg";
@@ -10,7 +11,9 @@ export const Home = () => {
     <div className="text-center mt-5">
       <img src={Finalfinal} className="headerlp" alt="headerlp" />
       <div className="boton">
-        <button className="btn btn-danger m-5">Contacto</button>
+        <Link to="/contacto">
+          <button className="btn btn-danger m-5 homebtn">Contacto</button>
+        </Link>
       </div>
     </div>
   );
