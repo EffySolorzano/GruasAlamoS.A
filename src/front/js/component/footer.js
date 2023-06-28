35a573e37f28ac1f29ed7a98af3458b6bd61f48e
Stephanie Solórzano <stephaniesolorzano@192.168.1.23>
2023-06-28 16:40:22 -0300

    db created

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 export const Footer = () => (
   <footer className="footer py-3 text-center">
-    <p className="ml-0">
+    <a className="ml-0" href="#">
       Grúas Alamo de Venezuela 2023
-      <i className="fa-sharp fa-regular fa-copyright"></i>
-    </p>
+      <i className="fa-sharp fa-regular fa-copyright"> </i>
+    </a>
     <a href="https://github.com/EffySolorzano">
+      {" "}
       Solórzano<i className="fa-solid fa-feather"></i>
     </a>
   </footer>
