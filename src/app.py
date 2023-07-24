57da2beb9d04698458d5dfde7e7f11545a464cd2
Stephanie Solórzano <stephaniesolorzano@192.168.1.4>
2023-07-24 17:56:30 -0300

    heroku deployment

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import Flask, request, jsonify, url_for, send_from_directory, render_template
+
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
@@ -11,6 +12,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+
 
 #from models import Person
 
