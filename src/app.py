bea457d476ed3bd907974cc93c174466fa1f0222
Stephanie Solórzano <stephaniesolorzano@192.168.1.4>
2023-07-24 20:37:56 -0300

    deploy

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
-from api.commands import setup_commands
+from api.commands import setup_commands 
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
